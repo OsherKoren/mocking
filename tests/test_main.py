75ddf6d96978b72fd1be9f6dde3316cfb33e7ff0
Oshrat <oshrats@gmail.com>
2022-02-11 15:56:28 +0200

    Added a test for main function

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+
+import main
+
+
+@pytest.mark.parametrize('test_level, expected',
+                         [
+                            (300, '2021-11-30'),
+                            (200, '2022-02-03'),
+                            (400, False),
+                         ]
+                         )
+def test_check_data(monkeypatch, setup_json, test_level, expected):
+    monkeypatch.setattr('data.fin_data.load_json', lambda *args, **kwargs: setup_json)
+    assert main.run_data_pipeline(level=test_level) == expected
